6da0f6049d671e0c25494c0c2f0e3f32542e98f5
anstaks <anstaks@yandex.ru>
2013-11-01 16:50:10 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Spree::Core::Engine.routes.draw do
-  match '/paysio_charges', :to => 'paysio_payments#paysio_charges', :as => :paysio_charges
-  match '/paysio_events', :to => 'paysio_payments#paysio_events', :as => :paysio_events
+  [:get, :post] '/paysio_charges', :to => 'paysio_payments#paysio_charges', :as => :paysio_charges
+  [:get, :post] '/paysio_events', :to => 'paysio_payments#paysio_events', :as => :paysio_events
 end
